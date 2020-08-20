bcaed6a8336f397475a553882f9cdbece421342c
fifthbeatle <dsouza.arnold@gmail.com>
2020-08-21 04:58:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const app = express()
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Welcome to building Node applications with Express JS!')
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+app.listen(port, () => {
+  console.log('Server is running on port ${port}')
 })
